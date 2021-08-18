b9e3a8854a901a62fc3091b574f9206c96c89986
Karen <thinknfix2021@gmail.com>
2021-08-18 19:52:08 +0800

    Update 2021-08-18T11:52:06.655Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,15 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); // Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 //# sourceMappingURL=all.js.map
