3a6dfe1380b432e6866b4df6087194afd1c550e2
Karen <thinknfix2021@gmail.com>
2021-08-18 19:51:19 +0800

    查看swiper在手機版上的狀態

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,16 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+// Initialize Swiper
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    
